a6d4bdec95636d6f52b5b5373ce36e6a3718bdf7
blackgik <echendu.chinedum@gmail.com>
2025-03-19 19:35:15 +0100

    Fixing debugs

--- dist/enum/connection.enum.js
+++
@@ -1,5 +0,0 @@
-export var knexModule;
-(function (knexModule) {
-    knexModule["PG"] = "pg";
-    knexModule["MYSQL"] = "mysql";
-})(knexModule || (knexModule = {}));
--- dist/update.js
+++
@@ -1,4 +0,0 @@
-export const updateItemSinDatabase = async (data, table, filterWith, dbconnections) => {
-    const updateData = await dbconnections(table).update(data).where(filterWith);
-    return { updateData, data, updated_at: new Date() };
-};
